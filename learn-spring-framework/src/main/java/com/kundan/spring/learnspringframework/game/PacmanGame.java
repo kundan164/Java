7a6bb76c209022b27cd7d6a6b4dee348ca551835
kundan164 <kundanprasad16@live.com>
2023-12-25 23:44:14 +0530

    spring demo

---
+++ learn-spring-framework/src/main/java/com/kundan/spring/learnspringframework/game/PacmanGame.java
@@ -0,0 +1,21 @@
+package com.kundan.spring.learnspringframework.game;
+
+public class PacmanGame implements GamingConsole {
+
+    public void up() {
+        System.out.println("PacmanGame up");
+    }
+
+    public void down() {
+        System.out.println("PacmanGame down");
+    }
+
+    public void left() {
+        System.out.println("PacmanGame left");
+    }
+
+    public void right() {
+        System.out.println("PacmanGame right");
+    }
+
+}
